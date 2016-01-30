e3a229e485fbec4e3b1527f5fdf4ca37138150b0
qiuyang57@gmail.com <qiuyang57@gmail.com>
2016-01-30 19:53:28 +0800

    ps5
    ps5 updated

---
+++ ps5/ps5.py
@@ -0,0 +1,309 @@
+# 6.00 Problem Set 5
+# RSS Feed Filter
+
+import feedparser
+import string
+import time
+from project_util import translate_html
+from news_gui import Popup
+
+#-----------------------------------------------------------------------
+#
+# Problem Set 5
+
+#======================
+# Code for retrieving and parsing
+# Google and Yahoo News feeds
+# Do not change this code
+#======================
+
+def process(url):
+    """
+    Fetches news items from the rss url and parses them.
+    Returns a list of NewsStory-s.
+    """
+    feed = feedparser.parse(url)
+    entries = feed.entries
+    ret = []
+    for entry in entries:
+        guid = entry.guid
+        title = translate_html(entry.title)
+        link = entry.link
+        summary = translate_html(entry.summary)
+        try:
+            subject = translate_html(entry.tags[0]['term'])
+        except AttributeError:
+            subject = ""
+        newsStory = NewsStory(guid, title, subject, summary, link)
+        ret.append(newsStory)
+    return ret
+
+#======================
+# Part 1
+# Data structure design
+#======================
+
+# Problem 1
+
+# TODO: NewsStory
+
+class NewsStory(object):
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
+    
+    def get_guid(self):
+        return self.guid
+    
+    def get_title(self):
+        return self.title
+
+    def get_subject(self):
+        return self.subject
+
+    def get_summary(self):
+        return self.summary
+
+    def get_link(self):
+        return self.link
+
+    def __str__(self):
+        return 'guid:' + self.guid + 'title:' + self.title + 'subject:' + self.subject + 'summary:' + self.summary + 'link:' + self.link
+    
+        
+
+#======================
+# Part 2
+# Triggers
+#======================
+
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        raise NotImplementedError
+
+# Whole Word Triggers
+# Problems 2-5
+
+# TODO: WordTrigger
+
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word
+
+    def is_word_in(self, text):
+        text_lower = text.lower()
+        word = self.word.lower()
+        for letter in text_lower: 
+            if letter in string.punctuation:
+                text_lower = text_lower.replace(letter,' ')
+        return word in text_lower.split()
+
+
+
+# TODO: TitleTrigger
+
+class TitleTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.get_title())
+
+# TODO: SubjectTrigger
+
+class SubjectTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.get_subject())
+
+# TODO: SummaryTrigger
+
+class SummaryTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.get_summary())
+
+
+# Composite Triggers
+# Problems 6-8
+
+# TODO: NotTrigger
+class NotTrigger(Trigger):
+    def __init__(self, trigger):
+        self.trigger = trigger
+        
+    def evaluate(self, story):
+        return not self.trigger.evaluate(story)
+    
+# TODO: AndTrigger
+class AndTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
+        
+# TODO: OrTrigger
+class OrTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) or self.trigger2.evaluate(story)
+
+
+
+# Phrase Trigger
+# Question 9
+
+# TODO: PhraseTrigger
+
+class PhraseTrigger(Trigger):
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+    def evaluate(self, story):
+        return self.phrase in story.get_subject() or self.phrase in story.get_title() or self.phrase in story.get_summary()
+
+#======================
+# Part 3
+# Filtering
+#======================
+
+def filter_stories(stories, triggerlist):
+    """
+    Takes in a list of NewsStory-s.
+    Returns only those stories for whom
+    a trigger in triggerlist fires.
+    """
+    # TODO: Problem 10
+    # This is a placeholder (we're just returning all the stories, with no filtering) 
+    # Feel free to change this line!
+    new_stories = []
+    for story in stories:
+        for trigger in triggerlist:
+            if trigger.evaluate(story):
+                new_stories.append(story)
+                break
+    return new_stories
+                
+
+#======================
+# Part 4
+# User-Specified Triggers
+#======================
+
+def make_trigger(name, trigger_type, parameter, trigger_map):
+    phrase = '' 
+    
+    if trigger_type == 'SUBJECT':
+        trigger_map[name] = SubjectTrigger(parameter[0])
+    elif trigger_type == 'TITLE':
+        trigger_map[name] = TitleTrigger(parameter[0])
+    elif trigger_type == 'SUMMARY':
+        trigger_map[name] = SummaryTrigger(parameter[0])
+    elif trigger_type == 'PHRASE':
+        trigger_map[name] = PhraseTrigger(' '.join(parameter))
+    elif trigger_type == 'NOT':
+        trigger_map[name] = NotTrigger(trigger_map[parameter[0]])
+    elif trigger_type == 'AND':
+        trigger_map[name] = AndTrigger(trigger_map[parameter[0]],
+                                       trigger_map[parameter[1]])
+    elif trigger_type == 'OR':
+        trigger_map[name] = OrTrigger(trigger_map[parameter[0]],
+                                       trigger_map[parameter[1]])
+    else:
+        return None
+        
+    
+def readTriggerConfig(filename):
+    """
+    Returns a list of trigger objects
+    that correspond to the rules set
+    in the file filename
+    """
+    # Here's some code that we give you
+    # to read in the file and eliminate
+    # blank lines and comments
+    triggerfile = open(filename, "r")
+    all = [ line.rstrip() for line in triggerfile.readlines() ]
+##    print all
+    lines = []
+    for line in all:
+        if len(line) == 0 or line[0] == '#':
+            continue
+        lines.append(line)
+
+    # TODO: Problem 11
+    # 'lines' has a list of lines you need to parse
+    # Build a set of triggers from it and
+    # return the appropriate ones
+    
+    trigger_map = {}
+    trigger_set = []
+    for line in lines:
+        word = line.split()
+        print word
+        if word[0] != 'ADD':
+            print word[0],word[1],word[2:]
+            make_trigger(word[0], word[1], word[2:], trigger_map)
+        else:
+            for i in word[1:]:
+                print trigger_map[i]
+                trigger_set.append(trigger_map[i])
+    return trigger_set
+    
+            
+
+    
+import thread
+
+def main_thread(p):
+    # A sample trigger list - you'll replace
+    # this with something more configurable in Problem 11
+##    t1 = SubjectTrigger("Obama")
+##    t2 = SummaryTrigger("MIT")
+##    t3 = PhraseTrigger("Supreme Court")
+##    t4 = OrTrigger(t2, t3)
+##    triggerlist = [t1, t4]
+    
+    # TODO: Problem 11
+    # After implementing readTriggerConfig, uncomment this line 
+    triggerlist = readTriggerConfig("triggers.txt")
+
+    guidShown = []
+    
+    while True:
+        print "Polling..."
+
+        # Get stories from Google's Top Stories RSS news feed
+        stories = process("http://news.google.com/?output=rss")
+        # Get stories from Yahoo's Top Stories RSS news feed
+        stories.extend(process("http://rss.news.yahoo.com/rss/topstories"))
+
+        # Only select stories we're interested in
+        stories = filter_stories(stories, triggerlist)
+    
+        # Don't print a story if we have already printed it before
+        newstories = []
+        for story in stories:
+            if story.get_guid() not in guidShown:
+                newstories.append(story)
+        
+        for story in newstories:
+            guidShown.append(story.get_guid())
+            p.newWindow(story)
+
+        print "Sleeping..."
+        time.sleep(SLEEPTIME)
+
+SLEEPTIME = 60 #seconds -- how often we poll
+if __name__ == '__main__':
+    p = Popup()
+    thread.start_new_thread(main_thread, (p,))
+    p.start()
+
